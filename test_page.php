0071d968c61d00cb9a18014f6a157f181c1bb9b4
Stefan Reindel <reindels@onid.orst.edu>
2014-11-18 18:24:22 -0800

    Made My Mark ~S

---
+++ test_page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+	<!-- 
+	HOW TO GET ON PUTTY GITHUB SHELL
+	-	
+	-	
+	-	
+	-	
+	HOW TO UP LOAD STUFF TO GITHUB
+	-	
+	-	Log to putty
+	-	cd pulbic_html/LaForge
+	-	git pull origin master
+	-	DO STUFF
+	-	git commit -m "Made My Mark ~S"    
+	-	git push origin master
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-	
+	-->
+	<head>
+		<title>TITLE</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+		<!-- Optional theme 
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">-->
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	</head>
+	
+	<body>
+		<h1>Lorem ipsum</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/></p>
+	</body>
+	<footer>
+		<p></p>
+	</footer>
+</html>
