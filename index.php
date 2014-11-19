f045444808158cbca6ba45e42c7108090b4fdbeb
Tyler Skelton <skeltont@onid.orst.edu>
2014-11-19 12:18:39 -0800

    added header, footer, & index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Inialize session
+session_start();
+// Check, if user is already logged in, then jump to secured page
+if(isset($_SESSION['username'])) {
+header('Location: login.php');
+}
+?>
+<?php $pagetitle = "Home Page"; ?>
+<?php $sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
+<?php $icon = "book_green.gif"; ?>
+<?php include $sitename."header.htm"; ?> 
+
+<div style="text-align:center;">
+	<p>
+	<img src="<?php echo $imgpath; ?>dsletters.jpg"/>
+	</p>
+</div>
+ <?php include $sitepath."footer.htm"; ?> 
