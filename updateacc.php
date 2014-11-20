946d0959d135846c67c1f6078d9fddb2beb4c40f
Colby Ham <colbyiphone@gmail.com>
2014-11-20 15:38:36 -0800

    Format updateacc.php

--- updateacc.php
+++ updateacc.php
@@ -12,26 +12,26 @@ header('Location: login.php');
 <?php include "header.htm"; ?> 
 <div style="text-align: center;">
 <h4>Add Child</h4>
-<form action="addchild.php" method="post">
-First Name: <input type="text" name="fname"><br>
-Last Name: <input type="text" name="lname"><br>
-<input type="submit">
-</form>
+	<form action="addchild.php" method="post">
+	First Name: <input type="text" name="fname"><br>
+	Last Name: <input type="text" name="lname"><br>
+	<input type="submit">
+	</form>
 <h4>Remove Child</h4>
-<form action="rmchild.php" method="post">
-First Name: <input type="text" name="fname"><br>
-Last Name: <input type="text" name="lname"><br>
-<input type="submit">
-</form>
+	<form action="rmchild.php" method="post">
+	First Name: <input type="text" name="fname"><br>
+	Last Name: <input type="text" name="lname"><br>
+	<input type="submit">
+	</form>
 <h4>Update Guardian Info</h4>
-<form action="update.php" method="post">
-First Name: <input type="text" name="fname"><br>
-Last Name: <input type="text" name="lname"><br>
-E-mail: <input type="text" name="email"><br>
-Current Password: <input type="text" name="cpass"><br>
-New Password: <input type="text" name="pass1"><br>
-Match New Password: <input type="text" name="pass2"><br>
-<input type="submit">
-</form>
+	<form action="update.php" method="post">
+	First Name: <input type="text" name="fname"><br>
+	Last Name: <input type="text" name="lname"><br>
+	E-mail: <input type="text" name="email"><br>
+	Current Password: <input type="text" name="cpass"><br>
+	New Password: <input type="text" name="pass1"><br>
+	Match New Password: <input type="text" name="pass2"><br>
+	<input type="submit">
+	</form>
 </div>
 <?php include "footer.htm"; ?> 
