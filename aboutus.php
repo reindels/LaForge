64d3a6dea14d91e1ee8aa7228da2e92118e77aec
Stefan Reindel <reindels@onid.orst.edu>
2014-11-20 17:53:22 -0800

    editted about us and contact

--- aboutus.php
+++ aboutus.php
@@ -8,10 +8,11 @@ header('Location: login.php');
 
 ?>
 <?php $pagetitle = "About Us"; ?>
-<?php $sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
+<?php //$sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
 <?php $icon = "book_green.gif"; ?>
 <?php include $sitename."header.htm"; ?> 
+	<h1>About us</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
 
-  
-
- <?php include $sitepath."footer.htm"; ?> 
+ <?php include /*$sitepath.*/"footer.htm"; ?> 
