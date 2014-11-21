2a3c68ca53ba3efa8c760356ece92c258da8c743
Stefan Reindel <reindels@onid.orst.edu>
2014-11-20 17:57:56 -0800

    editted index to not send itself on a one way trip to 404

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ if(isset($_SESSION['username'])) {
 header('Location: login.php');
 }
 ?>
-
-
 	<?php if(!isset($_SESSION['gid'])) { ?>
 		<form method="POST" action="">
 			<table>
@@ -21,26 +19,11 @@ header('Location: login.php');
 			</table>
 		</form>
 	<?php }else{ ?>
-	
-	
-	
 	<?php } ?>
-
-
-
-
-
-
-
-
-
-
-
-
 <?php $pagetitle = "Home Page"; ?>
-<?php $sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
+<?php //$sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
 <?php $icon = "book_green.gif"; ?>
-<?php include $sitepath."header.htm"; ?> 
+<?php include "header.htm"; ?> 
 
 <div style="text-align:center;">
 	<p>
@@ -48,13 +31,8 @@ header('Location: login.php');
 	</p>
 </div>
 
-
-
-
-
-
-// UNIT TEST TO SEE IF WE COULD QUERY FROM DATABASE
  <?php 
+	// UNIT TEST TO SEE IF WE COULD QUERY FROM DATABASE
 	// /* Select queries return a resultset */
 	// if ($result = $mysqli->query("SELECT * FROM Guardians")) {
 		// printf("Select returned %d rows.\n", $result->num_rows);
@@ -65,4 +43,4 @@ header('Location: login.php');
 		// $result->close();
 	// }
 ?>	
-<?php include $sitepath."footer.htm"; ?> 
+<?php include "footer.htm"; ?> 
