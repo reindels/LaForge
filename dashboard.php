746d15c5a740019875b56e0f78f7ede1e36b695f
Stefan Reindel <reindels@onid.orst.edu>
2014-11-21 13:36:54 -0800

    changed stuff in dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,8 +5,8 @@ session_start();
 if(isset($_SESSION['username'])) {
 header('Location: login.php');
 }
-
 ?>
+
 <?php $pagetitle = "Dashboard"; ?>
 <?php $sitepath="http://people.oregonstate.edu/~hamc/laforge/"; ?>
 <?php $icon = "book_green.gif"; ?>
@@ -15,21 +15,25 @@ header('Location: login.php');
 <table name = "subjects">
 
 <?php 
-   $SQLquery = "SELECT fname, lname FROM Dependents";
-   if(!$stmnt = $mysqli->prepare($SQLquery))
-      {echo "bad things happened $mysqli->connecterrno $mysqli->error";}
+	if ( isset($_SESSION['gid']) )
+	{
+	$SQLquery = "SELECT fname, lname FROM Dependents WHERE $_SESSION['gid'] == Dependents.gid";
+	if(!$stmnt = $mysqli->prepare($SQLquery))
+	{echo "bad things happened $mysqli->connecterrno $mysqli->error";}
 
-   if(!$stmnt->execute())
-      {echo "bad things 2 $mysqli->connecterrno $mysqli->error";}
+	if(!$stmnt->execute())
+	{echo "bad things 2 $mysqli->connecterrno $mysqli->error";}
 
-   if(!$stmnt->bind_result($fname,$lname))
-      {echo "even more bad things";}
-   while($stmnt->fetch()){
-      echo "<tr>";
-      echo "<td>$fname $lname</td>";
-      echo "</tr>";
-   }
-   $stmnt->close();
+	if(!$stmnt->bind_result($fname,$lname))
+	{echo "even more bad things";}
+	while($stmnt->fetch()){
+	echo "<tr>";
+	echo "<td>$fname $lname</td>";
+	echo "</tr>";
+	}
+	$stmnt->close();}
+	else
+	echo ("You are not logged in...");
 ?>
 </table>
    
