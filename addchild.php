d58cc1d4f1846c07111fc63db89c4afa30ad17f4
Colby Ham <colbyiphone@gmail.com>
2014-11-20 20:50:16 -0800

    SQL working addchild.php

--- addchild.php
+++ addchild.php
@@ -1,13 +1,25 @@
 <?php
-	session_start(); 
+	//session_start(); 
 	
-	if ($mysqli->connect_errno) {
-		printf("Connection failed: %s\n", $mysqli->connect_error);
-		exit();
-	}
+	$SQL_DIRECTORY = "oniddb.cws.oregonstate.edu";
+        $MY_USERNAME = "reindels-db";
+        $MY_PASSWORD = "QVUJJ2JUdAqBApEL";
+        $DATABASE_NAME = "reindels-db";
+        $mysqli = new mysqli($SQL_DIRECTORY,$MY_USERNAME,$MY_PASSWORD,$DATABASE_NAME);
+        if ($mysqli->connect_errno) {
+                printf("Connection failed: %s\n", $mysqli->connect_error);
+                exit();
+        }
+foreach ($_POST as $key => $value) {
+    //do something
+    echo $key . ' has the value of ' . $value;
+    echo '<br />';
+}
+
 	
-$fname = mysqli_real_escape_string ($_POST['fname']);
-$lname = mysqli_real_escape_string ($_POST['lname']);
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$gid = 1;
 
 $query = "INSERT INTO Dependents (fname, lname, gid) VALUES ('$fname', '$lname', '$gid')";
 
@@ -15,11 +27,9 @@ if ($fname == NULL || $lname == NULL) {
 		header('Location: error.php');
 }
 else {
-	if (!$result = $mysqli->query($q2)) {
-			die('There was an error['.$mysqli->error.']');
-			
-		}
-	else {
+	if (!$mysqli->query($query)) {
+			 printf("Error: %s\n", $mysqli->sqlstate);		
+	} else {
 		header('Location: updateacc.php');
 	}
 }
